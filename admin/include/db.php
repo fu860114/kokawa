6f1d519b33c79cfd868660e86139efa9dc2b0d5f
fu860114 <fu860114@gmail.com>
2024-08-01 17:40:23 +0800

    設定資料庫

--- admin/include/db.php
+++ admin/include/db.php
@@ -1,24 +1,19 @@
 <?php
 
-	if (!defined('Usable'))
-	{
-	    die('');
-	}
+if (!defined('Usable')) {
+	die('');
+}
 
-	//��Ʈw�]�w
-	$mysql_server   = "localhost";
-	$mysql_user     = "asiauibu_spa";
-	$mysql_password = "Kevin03141966";
-	$mysql_table    = "asiauibu_foody_spa";  // ���եθ�Ʈw 
-	//$mysql_db       = "rentcar";     // ���վ��θ�Ʈw 
+//��Ʈw�]�w
+$mysql_server   = "localhost";
+$mysql_user     = "kokawa";
+$mysql_password = "kokawaco_kokawa";
+$mysql_table    = "kokawaco_kokawa";  // ���եθ�Ʈw 
+//$mysql_db       = "rentcar";     // ���վ��θ�Ʈw 
 
-	if (!defined('DB_Ignore'))
-	{
+if (!defined('DB_Ignore')) {
 
-		//DB connect
-		$g_db = new Mysql( $mysql_server,$mysql_table,$mysql_user,$mysql_password,true );
-		$g_db->connectDB();
-
-	}
-
-?>
+	//DB connect
+	$g_db = new Mysql($mysql_server, $mysql_table, $mysql_user, $mysql_password, true);
+	$g_db->connectDB();
+}
--- admin/include/init.php
+++ admin/include/init.php
@@ -1,58 +1,54 @@
 <?
-    //echo "進入 init.php "."<BR>" ;  // 此行除錯用
-	session_start();
-    // echo "<script> alert('in init_david.php' )</script>";
-	if (!defined('Usable'))
-	{
-        // echo "沒有定義 Usable ,程式中斷 die " ;  // 此行除錯用
-   	    die('');
-	}
-	
-	//Tome Zone
-	if (version_compare( phpversion() , '5.1.0', '>=')){
-		date_default_timezone_set('Asia/Taipei'); //PHP5
-	} else {
-		putenv("TZ=Asia/Taipei"); //PHP4
-	}
+//echo "進入 init.php "."<BR>" ;  // 此行除錯用
+session_start();
+// echo "<script> alert('in init_david.php' )</script>";
+if (!defined('Usable')) {
+	// echo "沒有定義 Usable ,程式中斷 die " ;  // 此行除錯用
+	die('');
+}
+
+//Tome Zone
+if (version_compare(phpversion(), '5.1.0', '>=')) {
+	date_default_timezone_set('Asia/Taipei'); //PHP5
+} else {
+	putenv("TZ=Asia/Taipei"); //PHP4
+}
 
 
-	//  設定執行時的網站 ,gc_wbst 測試模式時設成 TETS ,正常機時 設成 RUN  
-   // define('gc_wbst', "TEST" );  //  內部測試主機時 設成 TETS 
-    define('gc_wbst', "RUN" );   //  正常對外主機時 設成 RUN  
-	
-	// echo "gc_wbst=".gc_wbst ;
-	// if(gc_wbst == "TEST") {
-	   // echo "TEST 模式" ;
-	// } else {
-	   // echo "RUN 模式" ;
-	// }		
-    // die();
-	//Define DIRECTORY_SEPARATOR    // 定義 目錄 的分隔符號
-	if (!defined('DS')) {
-    define('DS', DIRECTORY_SEPARATOR);
-	}
-    //Path  正式機環境 跟 開發測試環境不同 路徑要設對
-	
-	//Path
-	define('Root_Path',         '/home/asiauibu/public_html/web/food/spa/admin/');
-	define('Upload_Path',       '/home/asiauibu/public_html/web/food/spa/img/');
-	define('UploadDoc_Path',    '/home/asiauibu/public_html/web/food/spa/upload/');	
-	define('Root_Url',          'https://rental.yfmotor.com.tw/new2/');
-	define('Upload_Url',        'https://rental.yfmotor.com.tw/new2/upload/');
-	define('Https_Url',         'https://rental.yfmotor.com.tw/new2');
+//  設定執行時的網站 ,gc_wbst 測試模式時設成 TETS ,正常機時 設成 RUN  
+// define('gc_wbst', "TEST" );  //  內部測試主機時 設成 TETS 
+define('gc_wbst', "RUN");   //  正常對外主機時 設成 RUN  
+
+// echo "gc_wbst=".gc_wbst ;
+// if(gc_wbst == "TEST") {
+// echo "TEST 模式" ;
+// } else {
+// echo "RUN 模式" ;
+// }		
+// die();
+//Define DIRECTORY_SEPARATOR    // 定義 目錄 的分隔符號
+if (!defined('DS')) {
+	define('DS', DIRECTORY_SEPARATOR);
+}
+//Path  正式機環境 跟 開發測試環境不同 路徑要設對
+
+//Path
+define('Root_Path',         '/home/kokawaco/public_html/admin');
+define('Upload_Path',       '/home/kokawaco/public_html/img/');
+define('UploadDoc_Path',    '/home/kokawaco/public_html/upload/');
+define('Root_Url',          'https://rental.yfmotor.com.tw/new2/');
+define('Upload_Url',        'https://rental.yfmotor.com.tw/new2/upload/');
+define('Https_Url',         'https://rental.yfmotor.com.tw/new2');
 
 
-	//Vars	
-	//include_once(Root_Path . 'include' . DS . 'var.php');  david 2021/12/29 取消
+//Vars	
+//include_once(Root_Path . 'include' . DS . 'var.php');  david 2021/12/29 取消
 
-	//Include Tools
-	
-	//Include Class
-	include_once(Root_Path . 'include' . DS . 'class' . DS . 'mysql_david.php');           //MySQL
-    include_once(Root_Path . 'include' . DS . 'class' . DS . 'class.phpmailer.php'); //GoogleMail	
+//Include Tools
 
-	//Database
-	include_once(Root_Path . 'include' . DS . 'db.php');
-	
+//Include Class
+include_once(Root_Path . 'include' . DS . 'class' . DS . 'mysql_david.php');           //MySQL
+include_once(Root_Path . 'include' . DS . 'class' . DS . 'class.phpmailer.php'); //GoogleMail	
 
-?> 
+//Database
+include_once(Root_Path . 'include' . DS . 'db.php');
