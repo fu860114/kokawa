6889ed070768166c9cfb95092ddd5482cf586181
fu860114 <fu860114@gmail.com>
2024-08-08 21:08:10 +0800

    ddd

--- admin/product.php
+++ admin/product.php
@@ -147,6 +147,8 @@
 		$bidlist = $g_db->getAll($bidsqlstr);
 
 
+		echo "STOP2";
+		die();
 
 		//補名稱
 		for ($i = 0; $i < sizeof($list); $i++) {
@@ -160,10 +162,6 @@
 			$list[$i]["brand"] = $g_db->getOne($sqlstr);
 		}
 		$twig->addGlobal('list', $list);
-
-		echo "STOP2";
-		die();
-
 
 		// echo '<pre>' . var_export($list, true) . '</pre>';
 		// die();
