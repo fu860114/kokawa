610039486fd1f40fe2bdb8b0cab15aecc621b458
fu860114 <fu860114@gmail.com>
2024-08-09 20:27:01 +0800

    ccc

--- portfolio-single-collection.php
+++ portfolio-single-collection.php
@@ -33,7 +33,7 @@
     <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 <style>
-    @media screen and (min-width: 768px) and (max-width: 1199px){
+    @media screen and (min-width: 768px) and (max-width: 1199px) {
         .container {
             max-width: 95vw;
         }
@@ -50,249 +50,11 @@
 
     <div class="main-wrapper">
 
-        <!-- Header Section Start -->
-        <div class="header-section section">
-            <div class="container-fluid">
-                <div class="row align-items-center">
-
-                    <!-- Logo -->
-                    <div class="header-logo col-lg-2 col-6 mt-40 mb-40">
-                        <a href="index.html"><img src="assets/images/logo.png" alt=""><img
-                                src="assets/images/light-logo.png" class="light-logo" alt=""></a>
-                    </div>
-
-                    <!-- Header Menu -->
-                    <div class="header-menu d-lg-flex justify-content-center col-lg-8 d-none">
-                        <nav class="main-menu">
-                            <ul>
-                                <li class="menu-item-has-children"><a href="index.html">HOME</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="index.html">Home Default</a></li>
-                                        <li><a href="home-three-column.html">Home Three Column</a></li>
-                                        <li><a href="home-four-column.html">Home Four Column</a></li>
-                                        <li><a href="home-five-column.html">Home Five Column</a></li>
-                                        <li><a href="home-blog.html">Home Blog</a></li>
-                                        <li><a href="personal-portfolio.html">Personal Portfolio</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="about.html">ABOUT US</a></li>
-                                <li class="menu-item-has-children active"><a
-                                        href="portfolio-five-column-full-width.html">PORTFOLIO</a>
-                                    <ul class="sub-menu">
-                                        <li class="menu-item-has-children"><a href="#">Portfolio Grid</a>
-                                            <ul class="sub-menu">
-
-                                                <li><a href="portfolio-two-column-box.html">Two Column Box</a></li>
-                                                <li><a href="portfolio-three-column-box.html">Three Column Box</a></li>
-                                                <li><a href="portfolio-four-column-box.html">Four Column Box</a></li>
-
-                                                <li><a href="portfolio-three-column-full-width.html">Three Column
-                                                        Fullwidth</a></li>
-                                                <li><a href="portfolio-four-column-full-width.html">Four Column
-                                                        Fullwidth</a></li>
-                                                <li><a href="portfolio-five-column-full-width.html">Five Column
-                                                        Fullwidth</a></li>
-
-                                            </ul>
-                                        </li>
-                                        <li class="menu-item-has-children active"><a href="#">Portfolio Masonry</a>
-                                            <ul class="sub-menu">
-
-                                                <li class="active"><a href="portfolio-two-column-masonry-box.html">Two
-                                                        Column Box</a></li>
-                                                <li><a href="portfolio-three-column-masonry-box.html">Three Column
-                                                        Box</a></li>
-                                                <li><a href="portfolio-four-column-masonry-box.html">Four Column Box</a>
-                                                </li>
-
-                                                <li><a href="portfolio-three-column-masonry-full-width.html">Three
-                                                        Column Fullwidth</a></li>
-                                                <li><a href="portfolio-four-column-masonry-full-width.html">Four Column
-                                                        Fullwidth</a></li>
-                                                <li><a href="portfolio-five-column-masonry-full-width.html">Five Column
-                                                        Fullwidth</a></li>
-
-                                            </ul>
-                                        </li>
-                                        <li><a href="portfolio-details.html">Single Portfolio</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children"><a href="#">PAGES</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="service.html">service</a></li>
-                                        <li><a href="team.html">team</a></li>
-                                        <li><a href="contact.html">contact</a></li>
-                                        <li><a href="404.html">404 Error</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children"><a href="blog-three-column.html">BLOG</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="blog-three-column.html">Blog Three Column</a></li>
-                                        <li><a href="blog-two-column-left-sidebar.html">Blog two Column Left Sidebar</a>
-                                        </li>
-                                        <li><a href="blog-two-column-right-sidebar.html">Blog two Column Right
-                                                Sidebar</a></li>
-                                        <li><a href="blog-details-left-sidebar.html">Blog Details Left Sidebar</a></li>
-                                        <li><a href="blog-details-right-sidebar.html">Blog Details Right Sidebar</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a href="contact.html">CONTACT</a></li>
-                            </ul>
-                        </nav>
-                    </div>
-
-                    <!-- Header Socail -->
-                    <div class="header-social d-flex align-items-center justify-content-end col-lg-2 col-6">
-
-                        <a href="#"><i class="fa fa-facebook"></i></a>
-                        <a href="#"><i class="fa fa-twitter"></i></a>
-                        <a href="#"><i class="fa fa-dribbble"></i></a>
-
-                        <!-- Side Menu Toggle -->
-                        <button class="side-header-toggle d-block d-lg-none   ml-20"><span></span></button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Header Section End -->
-
-        <!-- Side Header Overlay Start -->
-        <div class="side-menu-overlay"></div>
-        <!-- Side Header Overlay End -->
-
-        <!-- Side Header Start -->
-        <div class="side-header custom-scroll mobile-side-header">
-
-            <!-- Side Header Inner Start -->
-            <div class="side-header-inner">
-                <button class="side-header-close d-block d-lg-none"><span></span></button>
-                <!-- Side Header Logo Start -->
-                <div class="side-header-logo pt-120 pt-lg-80 pt-md-80 pt-sm-80 pt-xs-50">
-                    <a href="index.html"><img src="assets/images/logo.png" alt=""><img
-                            src="assets/images/light-logo.png" class="light-logo" alt=""></a>
-                </div>
-                <!-- Side Header Logo End -->
-
-                <!-- Side Header Logo Start -->
-                <div class="side-header-menu">
-                    <nav id="side-menu" class="side-menu">
-                        <ul>
-                            <li class="menu-item-has-children"><a href="index.html">HOME</a>
-                                <ul class="side-sub-menu">
-                                    <li><a href="index.html">Home Default</a></li>
-                                    <li><a href="home-three-column.html">Home Three Column</a></li>
-                                    <li><a href="home-four-column.html">Home Four Column</a></li>
-                                    <li><a href="home-five-column.html">Home Five Column</a></li>
-                                    <li><a href="home-blog.html">Home Blog</a></li>
-                                    <li><a href="personal-portfolio.html">Personal Portfolio</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="about.html">ABOUT US</a></li>
-                            <li class="menu-item-has-children active"><a
-                                    href="portfolio-five-column-full-width.html">PORTFOLIO</a>
-                                <ul class="side-sub-menu">
-                                    <li class="menu-item-has-children"><a href="#">Portfolio Grid</a>
-                                        <ul class="side-sub-menu">
-
-                                            <li><a href="portfolio-two-column-box.html">Two Column Box</a></li>
-                                            <li><a href="portfolio-three-column-box.html">Three Column Box</a></li>
-                                            <li><a href="portfolio-four-column-box.html">Four Column Box</a></li>
-
-                                            <li><a href="portfolio-three-column-full-width.html">Three Column
-                                                    Fullwidth</a></li>
-                                            <li><a href="portfolio-four-column-full-width.html">Four Column
-                                                    Fullwidth</a></li>
-                                            <li><a href="portfolio-five-column-full-width.html">Five Column
-                                                    Fullwidth</a></li>
-
-                                        </ul>
-                                    </li>
-                                    <li class="menu-item-has-children active"><a href="#">Portfolio Masonry</a>
-                                        <ul class="side-sub-menu">
-
-                                            <li class="active"><a href="portfolio-two-column-masonry-box.html">Two
-                                                    Column Box</a></li>
-                                            <li><a href="portfolio-three-column-masonry-box.html">Three Column Box</a>
-                                            </li>
-                                            <li><a href="portfolio-four-column-masonry-box.html">Four Column Box</a>
-                                            </li>
-
-                                            <li><a href="portfolio-three-column-masonry-full-width.html">Three Column
-                                                    Fullwidth</a></li>
-                                            <li><a href="portfolio-four-column-masonry-full-width.html">Four Column
-                                                    Fullwidth</a></li>
-                                            <li><a href="portfolio-five-column-masonry-full-width.html">Five Column
-                                                    Fullwidth</a></li>
-
-                                        </ul>
-                                    </li>
-                                    <li><a href="portfolio-details.html">Single Portfolio</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item-has-children"><a href="#">PAGES</a>
-                                <ul class="side-sub-menu">
-                                    <li><a href="service.html">service</a></li>
-                                    <li><a href="team.html">team</a></li>
-                                    <li><a href="contact.html">contact</a></li>
-                                    <li><a href="404.html">404 Error</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item-has-children"><a href="blog-three-column.html">BLOG</a>
-                                <ul class="side-sub-menu">
-                                    <li><a href="blog-three-column.html">Blog Three Column</a></li>
-                                    <li><a href="blog-two-column-left-sidebar.html">Blog two Column Left Sidebar</a>
-                                    </li>
-                                    <li><a href="blog-two-column-right-sidebar.html">Blog two Column Right Sidebar</a>
-                                    </li>
-                                    <li><a href="blog-details-left-sidebar.html">Blog Details Left Sidebar</a></li>
-                                    <li><a href="blog-details-right-sidebar.html">Blog Details Right Sidebar</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="contact.html">CONTACT</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <!-- Side Header Menu End -->
-
-                <!-- Side Header Logo Start -->
-                <div class="side-header-footer pb-120 pb-lg-80 pb-md-80 pb-sm-80 pb-xs-50">
-
-                    <!-- Side Header Contact Information -->
-                    <div class="side-header-contact-info">
-                        <p><a href="#">+012 345 6789</a></p>
-                        <p><a href="#">info@example.com</a></p>
-                    </div>
-
-                    <!-- Side Header Social -->
-                    <div class="side-header-social">
-                        <a href="#"><i class="fa fa-facebook"></i></a>
-                        <a href="#"><i class="fa fa-twitter"></i></a>
-                        <a href="#"><i class="fa fa-dribbble"></i></a>
-                    </div>
-
-                    <!-- Side Header Copyright -->
-                    <div class="side-header-copyright">
-                        <p>&copy;
-                            <script>
-                                document.write(new Date().getFullYear() + ' ');
-                            </script> MINIMAU. ALL RIGHT RESERVED
-                        </p>
-                    </div>
-
-                </div>
-                <!-- Side Header Footer End -->
-
-            </div>
-            <!-- Side Header Inner End -->
-
-        </div>
-        <!-- Side Header End -->
+        <?php include('./tpl/head.php'); ?>
 
         <!-- Content Body Start -->
         <div class="content-body bg-grey section custom-scroll">
-            <div
-                class="section-wrap section pt-120 pt-lg-80 pt-md-80 pt-sm-80 pt-xs-50 pb-120 pb-lg-80 pb-md-80 pb-sm-80 pb-xs-50">
+            <div class="section-wrap section pt-120 pt-lg-80 pt-md-80 pt-sm-80 pt-xs-50 pb-120 pb-lg-80 pb-md-80 pb-sm-80 pb-xs-50">
                 <div class="container">
 
                     <div class="row row-7 portfolio-column-two masonry-grid">
@@ -545,7 +307,7 @@
         <!-- Content Body End -->
 
         <!-- Footer Section Start -->
-        <div class="footer-section section pt-30 pb-30">
+        <!-- <div class="footer-section section pt-30 pb-30">
             <div class="container-fluid">
                 <div class="row align-items-center">
 
@@ -570,7 +332,7 @@
 
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- Footer Section End -->
 
     </div>
